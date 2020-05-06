fa32e93475dff23b9a151da38c797a29df666446
Rafael Souza <raffaelsouza908@gmail.com>
2020-05-05 23:59:41 -0300

    Remove uma transação

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.findOne({ where: { id } });
+
+    if (!transaction) {
+      throw new AppError('No transaction was found with this id');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
